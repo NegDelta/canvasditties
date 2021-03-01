9e70178d318e2c2add37deb2b0ca9f30706e56af
delta <pchmiel93@gmail.com>
2021-03-01 23:35:29 +0100

    Introducing Map class

--- maincell.js
+++ maincell.js
@@ -3,3 +3,25 @@ var ctx = canv.getContext("2d");
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canv.width, canv.height);
 
+class Map {
+	constructor() {
+		this.cells = {};
+	}
+	default = false;
+	get(x, y) {
+		if (!(y in this.cells.keys())) {
+			return this.default;
+		}
+		if (!(x in this.cells[y].keys())) {
+			return this.default;
+		}
+		return this.cells[y][x];
+	}
+	set(x, y, val) {
+		if (!(y in this.cells.keys())) {
+			this.cells[y] = {};
+		}
+		this.cells[y][x] = val;
+	}
+}
+
