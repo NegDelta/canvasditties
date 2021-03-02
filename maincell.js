c0d39ea8250ca92780eb93e57993ee26eba719c9
delta <pchmiel93@gmail.com>
2021-03-02 22:56:43 +0100

    Set up event dispatching, fixes

--- maincell.js
+++ maincell.js
@@ -3,25 +3,36 @@ var ctx = canv.getContext("2d");
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canv.width, canv.height);
 
+const frameevent = new Event("frame");
+
 class Map {
 	constructor() {
 		this.cells = {};
 	}
 	default = false;
 	get(x, y) {
-		if (!(y in this.cells.keys())) {
+		if (!(y in this.cells)) {
 			return this.default;
 		}
-		if (!(x in this.cells[y].keys())) {
+		if (!(x in this.cells[y])) {
 			return this.default;
 		}
 		return this.cells[y][x];
 	}
 	set(x, y, val) {
-		if (!(y in this.cells.keys())) {
+		if (!(y in this.cells)) {
 			this.cells[y] = {};
 		}
 		this.cells[y][x] = val;
+		canv.dispatchEvent(frameevent);
 	}
 }
+
+var M = new Map();
+
+function canvupdate() {
+	console.log("Frame! :D");
+}
+
+canv.addEventListener("frame", canvupdate);
 
